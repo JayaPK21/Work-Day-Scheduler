6db4131ca1eaf7df7aa180c622df7eede4e62202
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-01-19 11:23:21 +0000

    Adds the correct background color for each time block in the application

---
+++ assets/js/scheduler.js
@@ -0,0 +1,31 @@
+var currentDayEl = $('#currentDay');
+
+var timeBlocks = $('.time-block');
+var today = moment();
+var currentHour = Number(today.format('H'));
+
+currentDayEl.text(today.format('dddd, MMMM Do YYYY'));
+
+//console.log(timeBlocks);
+
+$.each(timeBlocks, function (i, val) {
+    
+    // As the workday starts at 9 am, adding 9 to the index to compare with the current hour.
+    var blockHour = i + 9;
+
+    // Checks if each time block is in the past, present or future hour of the day.
+    if(blockHour < currentHour){
+
+        // The appropriate class is added to set the correct background for the corresponding time block.
+        $(val).children('textarea').addClass('past');
+
+    } else if(blockHour == currentHour) {
+
+        $(val).children('textarea').addClass('present');
+
+    } else {
+        
+        $(val).children('textarea').addClass('future');
+    }
+});
+
